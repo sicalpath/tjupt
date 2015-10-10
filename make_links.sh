f34f41888ee10bd1ce3d9d8a463b1e703d8f96fc
Zhongchao Qian <squid157@gmail.com>
2015-10-10 15:17:38 +0800

    move config from code to data (remove config file)

--- make_links.sh
+++ make_links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in attachments banners bitbucket subs tmp torrents upload
+for i in attachments banners bitbucket config subs tmp torrents upload
 do
 ln -sv "/data/pt.tju.edu.cn/$i"
 done
