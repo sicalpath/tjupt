6e24f647146337c72fb17ec42600fa9ca0c8f86d
Zhongchao Qian <squid157@gmail.com>
2016-01-07 16:14:46 +0800

    add function get_db_handle

--- include/next_base.php
+++ include/next_base.php
@@ -3,5 +3,20 @@
  * Base utility functions for next gen pt
  * (c)2016 Zhongchao Qian
  */
-if (! defined ( 'IN_TRACKER' ))
-    die ( 'Hacking attempt!' );
+
+
+$rootpath =  realpath(dirname(__FILE__) . '../');
+
+include_once $rootpath . 'include/next_nexus_cfg.php';
+
+function get_db_handle(){
+    try{
+        $dbh = new PDO(PtConfig\MYSQL_DSN, PtConfig\MYSQL_USER, PtConfig\MYSQL_PASS,
+            array(
+                PDO::ATTR_PERSISTENT => true
+            ));
+        return $dbh;
+    } catch (PDOException $e) {
+        die("Failed to connect to DB!");
+    }
+}
