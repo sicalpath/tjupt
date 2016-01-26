8249511af2a62ff66d8c3b2bb326f14a8c6dd980
Zhongchao Qian <squid157@gmail.com>
2016-01-26 16:43:37 +0800

    make PT_DATADIR a variable

--- make_links.sh
+++ make_links.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+PT_DATADIR="/pt-data/tjupt-data"
+
 for i in attachments banners bitbucket config subs tmp torrents upload
 do
-ln -sv "/data/pt.tju.edu.cn/$i"
+ln -sv "$PT_DATADIR/pt.tju.edu.cn/$i"
 done
 
-ln -sv "/data/pt.tju.edu.cn/imdb/cache" "imdb/"
+ln -sv "$PT_DATADIR/imdb/cache" "imdb/"
 
