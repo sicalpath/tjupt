e4e57263fe846f0a10f2a0e8caef49e9688b6256
Zhongchao Qian <squid157@gmail.com>
2016-01-26 16:49:52 +0800

    fix nasty typo/misreplacement

--- make_links.sh
+++ make_links.sh
@@ -4,7 +4,7 @@ PT_DATADIR="/pt-data/tjupt-data"
 
 for i in attachments banners bitbucket config subs tmp torrents upload
 do
-ln -sv "$PT_DATADIR/pt.tju.edu.cn/$i"
+ln -sv "$PT_DATADIR/$i"
 done
 
 ln -sv "$PT_DATADIR/imdb/cache" "imdb/"
