bb0c052dd3cd6f3f05cb0560795f16250e907261
Zhongchao Qian <squid157@gmail.com>
2016-02-14 07:27:32 +0800

    remove unused files

--- testwho.php
+++
@@ -1,9 +0,0 @@
-<?
-//include('../include/core.php');
-//include('../include/functions_announce.php');
-//$global_promotion_state = get_global_sp_state();
-//echo $global_promotion_state;
-echo $_SERVER["PHP_SELF"];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'] ;
-?>
